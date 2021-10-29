6555c76b671852a40e2adae1a0328fc0227caa2f
philippemnoel <21990816+philippemnoel@users.noreply.github.com>
2021-10-29 18:17:09 +0000

    Update protocol documentation (https://github.com/fractal/fractal/commit/9cd531e5ea52a7c5abe4c1e21c8ce6e83e21170f)

--- protocol/search/enumvalues_9.js
+++ protocol/search/enumvalues_9.js
@@ -1,12 +1,12 @@
 var searchData=
 [
-  ['ne_5fcursor_5fhash_3872',['NE_CURSOR_HASH',['../linuxcursor_8c.html#a9a44d7a1f5d95832e3cbce8676afa768adfc34344de8211ce9da7927b90200342',1,'linuxcursor.c']]],
-  ['nesw_5fcursor_5fhash_3873',['NESW_CURSOR_HASH',['../linuxcursor_8c.html#a9a44d7a1f5d95832e3cbce8676afa768a818614cbe58b7010ee7e076acb73aaa6',1,'linuxcursor.c']]],
-  ['not_5fallowed_5fcursor_5fhash_3874',['NOT_ALLOWED_CURSOR_HASH',['../linuxcursor_8c.html#a9a44d7a1f5d95832e3cbce8676afa768a47b9da7d77958040c28310c7a5c93551',1,'linuxcursor.c']]],
-  ['ns_5fcursor_5fhash_3875',['NS_CURSOR_HASH',['../linuxcursor_8c.html#a9a44d7a1f5d95832e3cbce8676afa768aa7f345948486a6668a6e10e93f370a12',1,'linuxcursor.c']]],
-  ['nvenc_5fencode_3876',['NVENC_ENCODE',['../ffmpeg__encode_8h.html#a710867b9460bb3ef175428fd1da3d41aa4eaed9aa42e53ca6e33b4d923ff362a5',1,'ffmpeg_encode.h']]],
-  ['nvidia_5fdevice_3877',['NVIDIA_DEVICE',['../fractal_8h.html#a405cbf3c7e9ae25dc03ad4fcf563f600abaa3f428485672d8d26eff1d9b02471c',1,'fractal.h']]],
-  ['nvidia_5fencoder_3878',['NVIDIA_ENCODER',['../videoencode_8h.html#a2f3c0233caf50b001eb28cbaea7f0562ab792692a01f6b3a964743a59ae2cbec4',1,'videoencode.h']]],
-  ['nw_5fcursor_5fhash_3879',['NW_CURSOR_HASH',['../linuxcursor_8c.html#a9a44d7a1f5d95832e3cbce8676afa768a866642bed89a1a54ed31cddb74aa4d2f',1,'linuxcursor.c']]],
-  ['nwse_5fcursor_5fhash_3880',['NWSE_CURSOR_HASH',['../linuxcursor_8c.html#a9a44d7a1f5d95832e3cbce8676afa768a60ce484b6fa427a1406c177c3d781f25',1,'linuxcursor.c']]]
+  ['ne_5fcursor_5fhash_3860',['NE_CURSOR_HASH',['../linuxcursor_8c.html#a9a44d7a1f5d95832e3cbce8676afa768adfc34344de8211ce9da7927b90200342',1,'linuxcursor.c']]],
+  ['nesw_5fcursor_5fhash_3861',['NESW_CURSOR_HASH',['../linuxcursor_8c.html#a9a44d7a1f5d95832e3cbce8676afa768a818614cbe58b7010ee7e076acb73aaa6',1,'linuxcursor.c']]],
+  ['not_5fallowed_5fcursor_5fhash_3862',['NOT_ALLOWED_CURSOR_HASH',['../linuxcursor_8c.html#a9a44d7a1f5d95832e3cbce8676afa768a47b9da7d77958040c28310c7a5c93551',1,'linuxcursor.c']]],
+  ['ns_5fcursor_5fhash_3863',['NS_CURSOR_HASH',['../linuxcursor_8c.html#a9a44d7a1f5d95832e3cbce8676afa768aa7f345948486a6668a6e10e93f370a12',1,'linuxcursor.c']]],
+  ['nvenc_5fencode_3864',['NVENC_ENCODE',['../ffmpeg__encode_8h.html#a710867b9460bb3ef175428fd1da3d41aa4eaed9aa42e53ca6e33b4d923ff362a5',1,'ffmpeg_encode.h']]],
+  ['nvidia_5fdevice_3865',['NVIDIA_DEVICE',['../fractal_8h.html#a405cbf3c7e9ae25dc03ad4fcf563f600abaa3f428485672d8d26eff1d9b02471c',1,'fractal.h']]],
+  ['nvidia_5fencoder_3866',['NVIDIA_ENCODER',['../videoencode_8h.html#a2f3c0233caf50b001eb28cbaea7f0562ab792692a01f6b3a964743a59ae2cbec4',1,'videoencode.h']]],
+  ['nw_5fcursor_5fhash_3867',['NW_CURSOR_HASH',['../linuxcursor_8c.html#a9a44d7a1f5d95832e3cbce8676afa768a866642bed89a1a54ed31cddb74aa4d2f',1,'linuxcursor.c']]],
+  ['nwse_5fcursor_5fhash_3868',['NWSE_CURSOR_HASH',['../linuxcursor_8c.html#a9a44d7a1f5d95832e3cbce8676afa768a60ce484b6fa427a1406c177c3d781f25',1,'linuxcursor.c']]]
 ];
--- protocol/search/enumvalues_c.js
+++ protocol/search/enumvalues_c.js
@@ -1,13 +1,13 @@
 var searchData=
 [
-  ['se_5fcursor_5fhash_3886',['SE_CURSOR_HASH',['../linuxcursor_8c.html#a9a44d7a1f5d95832e3cbce8676afa768adc8fba076ca60b5f7be3440a6f32e2d6',1,'linuxcursor.c']]],
-  ['smessage_5fclipboard_3887',['SMESSAGE_CLIPBOARD',['../fractal_8h.html#aa56483762a863ec73e68c53c8fdf140ca86b5ce811becb248c9572438105db9aa',1,'fractal.h']]],
-  ['smessage_5ffullscreen_3888',['SMESSAGE_FULLSCREEN',['../fractal_8h.html#aa56483762a863ec73e68c53c8fdf140ca8bee600907e82ec0da4091d1c1a5abe5',1,'fractal.h']]],
-  ['smessage_5fnone_3889',['SMESSAGE_NONE',['../fractal_8h.html#aa56483762a863ec73e68c53c8fdf140cafcd5317ecd005a890b1c3e70c96f5695',1,'fractal.h']]],
-  ['smessage_5fopen_5furi_3890',['SMESSAGE_OPEN_URI',['../fractal_8h.html#aa56483762a863ec73e68c53c8fdf140ca8aa255189138c9a25497f6e544c8f645',1,'fractal.h']]],
-  ['smessage_5fquit_3891',['SMESSAGE_QUIT',['../fractal_8h.html#aa56483762a863ec73e68c53c8fdf140ca62adb843bfd6857bbf5f634425c404d1',1,'fractal.h']]],
-  ['smessage_5fwindow_5ftitle_3892',['SMESSAGE_WINDOW_TITLE',['../fractal_8h.html#aa56483762a863ec73e68c53c8fdf140ca350f6ac715ca8fba37c2967385fd1594',1,'fractal.h']]],
-  ['software_5fencode_3893',['SOFTWARE_ENCODE',['../ffmpeg__encode_8h.html#a710867b9460bb3ef175428fd1da3d41aa5f0e0a33b7479565a2829cde8be8b24e',1,'ffmpeg_encode.h']]],
-  ['spectate_3894',['SPECTATE',['../fractal_8h.html#acf8e584d95cf66831f54e2d9c933b2ffa01da13585122415fbe64a1d4961fcd35',1,'fractal.h']]],
-  ['sw_5fcursor_5fhash_3895',['SW_CURSOR_HASH',['../linuxcursor_8c.html#a9a44d7a1f5d95832e3cbce8676afa768a2eaca2d8017b22859c54aa2dcb33ed41',1,'linuxcursor.c']]]
+  ['se_5fcursor_5fhash_3874',['SE_CURSOR_HASH',['../linuxcursor_8c.html#a9a44d7a1f5d95832e3cbce8676afa768adc8fba076ca60b5f7be3440a6f32e2d6',1,'linuxcursor.c']]],
+  ['smessage_5fclipboard_3875',['SMESSAGE_CLIPBOARD',['../fractal_8h.html#aa56483762a863ec73e68c53c8fdf140ca86b5ce811becb248c9572438105db9aa',1,'fractal.h']]],
+  ['smessage_5ffullscreen_3876',['SMESSAGE_FULLSCREEN',['../fractal_8h.html#aa56483762a863ec73e68c53c8fdf140ca8bee600907e82ec0da4091d1c1a5abe5',1,'fractal.h']]],
+  ['smessage_5fnone_3877',['SMESSAGE_NONE',['../fractal_8h.html#aa56483762a863ec73e68c53c8fdf140cafcd5317ecd005a890b1c3e70c96f5695',1,'fractal.h']]],
+  ['smessage_5fopen_5furi_3878',['SMESSAGE_OPEN_URI',['../fractal_8h.html#aa56483762a863ec73e68c53c8fdf140ca8aa255189138c9a25497f6e544c8f645',1,'fractal.h']]],
+  ['smessage_5fquit_3879',['SMESSAGE_QUIT',['../fractal_8h.html#aa56483762a863ec73e68c53c8fdf140ca62adb843bfd6857bbf5f634425c404d1',1,'fractal.h']]],
+  ['smessage_5fwindow_5ftitle_3880',['SMESSAGE_WINDOW_TITLE',['../fractal_8h.html#aa56483762a863ec73e68c53c8fdf140ca350f6ac715ca8fba37c2967385fd1594',1,'fractal.h']]],
+  ['software_5fencode_3881',['SOFTWARE_ENCODE',['../ffmpeg__encode_8h.html#a710867b9460bb3ef175428fd1da3d41aa5f0e0a33b7479565a2829cde8be8b24e',1,'ffmpeg_encode.h']]],
+  ['spectate_3882',['SPECTATE',['../fractal_8h.html#acf8e584d95cf66831f54e2d9c933b2ffa01da13585122415fbe64a1d4961fcd35',1,'fractal.h']]],
+  ['sw_5fcursor_5fhash_3883',['SW_CURSOR_HASH',['../linuxcursor_8c.html#a9a44d7a1f5d95832e3cbce8676afa768a2eaca2d8017b22859c54aa2dcb33ed41',1,'linuxcursor.c']]]
 ];
