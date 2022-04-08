4bd9e0e230ea6d2001e19c882476d4983b86f229
philippemnoel <21990816+philippemnoel@users.noreply.github.com>
2022-04-08 17:51:49 +0000

    Update Whist internal documentation (https://github.com/whisthq/whist/commit/551cb80804f4ba8515395a0475938b92927c3362)

--- protocol/structAudioDecoder.js
+++ protocol/structAudioDecoder.js
@@ -4,6 +4,6 @@ var structAudioDecoder =
     [ "context", "structAudioDecoder.html#aff3ae75c61f4666b4347763ca6ac9f3d", null ],
     [ "frame", "structAudioDecoder.html#afa1eb71c5c3a651f012993d7838fe994", null ],
     [ "out_buffer", "structAudioDecoder.html#a827784bdad17fdd71ab71de887f34837", null ],
-    [ "packets", "structAudioDecoder.html#a4a55dceb13b37b190d1e4f8e9743f794", null ],
+    [ "packets", "structAudioDecoder.html#a10a981c8ee6251b566ef6dbda5aa83e7", null ],
     [ "swr_context", "structAudioDecoder.html#ac82173b481da4e3df0fa9d208faca4f2", null ]
 ];
