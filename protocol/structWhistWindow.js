03c44a1dab8feb610bc8937af1ac89240ff353ef
npip99 <npip99@gmail.com>
2022-05-30 16:12:04 +0000

    Update Whist internal documentation (https://github.com/whisthq/whist/commit/74016d16451878626e3ec1647a78329ed6144233)

--- protocol/structWhistWindow.js
+++ protocol/structWhistWindow.js
@@ -2,7 +2,7 @@ var structWhistWindow =
 [
     [ "corner_color", "structWhistWindow.html#aa0d3a979031d759e85594e5c7773c1d1", null ],
     [ "height", "structWhistWindow.html#a73dc92a019f0c1121db35eb4a7b21022", null ],
-    [ "id", "structWhistWindow.html#ad9372e55cfd4132f29173a883b19bbfc", null ],
+    [ "id", "structWhistWindow.html#ae15a0784ef3b009825a150df3bf6f86d", null ],
     [ "is_fullscreen", "structWhistWindow.html#adadd3a20bc1ece5f72730a213169f5d0", null ],
     [ "LINKED_LIST_HEADER", "structWhistWindow.html#a5eeb32d49ab1899ccc47902930d7c436", null ],
     [ "width", "structWhistWindow.html#a50499c2b085fd212662787a3ef8f412e", null ],
