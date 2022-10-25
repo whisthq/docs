cfe6c7a2c84efea33f69468a4de484e6f5b6d4b5
philippemnoel <phil@whist.com>
2022-10-25 23:01:14 +0000

    Update Whist internal documentation (https://github.com/whisthq/whist/commit/180ad7b0ffc36b82c064c62002c254396a0627a5)

--- protocol/plotter_8h.js
+++ protocol/plotter_8h.js
@@ -1,8 +1,8 @@
 var plotter_8h =
 [
-    [ "whist_plotter_export_c", "plotter_8h.html#a3aa4f3beb43e4618c87fc36d05a6836a", null ],
+    [ "whist_plotter_destroy", "plotter_8h.html#aa48551df1572bc70825448c00e59fb47", null ],
     [ "whist_plotter_export_to_file", "plotter_8h.html#a1a4837bf96cee54300991e88f253d8b2", null ],
-    [ "whist_plotter_init", "plotter_8h.html#ae197a58f4f3d4176640cbb3a6644a2ab", null ],
+    [ "whist_plotter_init", "plotter_8h.html#a454fe0b8f758996e40f8e4bfd88ac9e6", null ],
     [ "whist_plotter_insert_sample", "plotter_8h.html#a940485ae6fc5690c9776b199303e46ab", null ],
     [ "whist_plotter_start_sampling", "plotter_8h.html#a1dea4345f9e00dac5a71338ceff39299", null ],
     [ "whist_plotter_stop_sampling", "plotter_8h.html#abff1d213f322b7d7c83c711bfa0ad102", null ]
